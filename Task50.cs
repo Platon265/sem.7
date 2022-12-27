961947f452012f23347d7de68119a798ccd5cc65
Platon265 <119070658+Platon265@users.noreply.github.com>
2022-12-27 19:12:34 +0300

    Create Task50.cs

---
+++ Task50.cs
@@ -0,0 +1,41 @@
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] mainMatrix = Create2DArray(m, n, -10, 10);
+Print2DArray(mainMatrix);
+
+Console.WriteLine("Введите номер строки");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите номер столбца");
+int col = int.Parse(Console.ReadLine()!);
+
+if (row > m || col > n) Console.WriteLine("такого числа в массиве нет");
+else Console.WriteLine(mainMatrix[row - 1, col - 1]);
+
+int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
